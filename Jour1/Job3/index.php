50a8fc158a443f8db1760c6e64bd5a4faf73d839
Alexandre Chikhaoui <alexandre.chikhaoui@laplateforme.io>
2025-07-06 19:27:28 +0200

    first commit

---
+++ Jour1/Job3/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma page avec js externe</title>
+    <script src="script.js" ></script>
+</head>
+<body>
+    <h1>Bienvenue sur ma page PHP</h1>
+    <p>Le message JavaScript s'affiche dans la console..</p>
+</body>
+</html>
