762ad2752c365ae784ce21b3201323bbd2be9dcc
Alexandre Chikhaoui <alexandre.chikhaoui@laplateforme.io>
2025-09-22 11:42:48 +0200

    runtrack 3 js

---
+++ Jour1/Job6/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1> Fonction Fizzbuzz</h1>
+    <p>Appuiyer sur F12 pour affichage du resultat</p>
+</body>
+</html>
